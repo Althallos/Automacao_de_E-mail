9c3c372428cfaa9a8f57bb57409a5bf5f46a3047
ZhouZKZ <roberto16092004@gmail.com>
2023-10-23 23:36:35 -0300

    Gerador de Arquivos CSV

---
+++ Projeto/Gerador_CSV.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from pathlib import Path
+
+#       Gerador de Arquivos CSV
+
+#   OBS: Caso de erro de arquivo Insira o caminho do arquivo
+
+# Criando a pasta Relatorios
+pasta_relatorio = Path(r'Projeto/Relatorios').mkdir()
+
+# Cria o CSV Enviar_E-mail
+
+lista = {'Gerente':['Maria', 'Lira', 'Guilherme', 'Pedro', 'Júlia', 'Larrisa', 'Sabrina'],
+         'E-mail':['user+1@gmail.com', 'user+2@gmail.com', 'user+3@gmail.com', 'user+4@gmail.com', 'user+5@gmail.com', 'user+6@gmail.com', 'user+7@gmail.com'],
+         'Relatório':['Financeiro', 'Logística', 'Manuntenção', 'Marketing', 'Operações', 'Produtos', 'Vendas']}
+arquivo = pd.DataFrame(lista)
+arquivo.to_csv(r'Projeto/Enviar_E-mail.csv', index=False)
+
+relatorio_csv = pd.DataFrame()
+# Cria arquivos CSV para cada relatório
+for relatorio in arquivo["Relatório"]:
+    relatorio_csv.to_csv(r'Projeto/Relatorios/{}.csv'.format(relatorio))
